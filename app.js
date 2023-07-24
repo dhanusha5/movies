dc33db3e59faf2010e008b7debfaa64eaa062b6e
dhanusha <dhanusha5461@gmail.com>
2023-07-24 15:55:07 +0000

    solution

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "moviesData.db");
+
+const app = express();
+app.use(express.json());
+
+let database = null;
+
+const intializeDbAndServer = async () => {
+  try {
+    database = await open({
+      fileName: databasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3001, () => console.log("server running"));
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+intializeDbAndServer();
+
+const convertMovieObjectToResponseObject = (dbObject) => {
+  return {
+    movieId: dbObject.movie_id,
+    directorId: dbObject.director_id,
+    movieName: dbObject.movie_name,
+    leadActor: dbObject.lead_actor,
+  };
+};
+
+const convertDirectorObjectToResponseObject = (dbObject) => {
+  return {
+    directorId: dbObject.director_id,
+    directorName: dbObject.director_name,
+  };
+};
+
+app.get("/movies/", async (request, response) => {
+  const getMoviesQuery = `
+     SELECT
+     movie_name
+     FROM
+     movie;`;
+  const moviesArray = await database.all(getMoviesQuery);
+  response.send(
+    moviesArray.map((eachMovie) => ({
+      movieName: eachMovie.movie_name,
+    }))
+  );
+});
