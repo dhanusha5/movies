d9042cb0532ee55d5ef524b2095c8a8cdeefb01a
dhanusha5 <137540535+dhanusha5@users.noreply.github.com>
2023-07-31 14:29:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -66,4 +66,73 @@ app.post("/movies/", async (request, response) => {
   response.send("Movie Successfully Added");
 });
 
+app.get("/movies/:movieId", async (request, response) => {
+  const { movieId } = request.params;
+  const getMovieQuery = `
+    SELECT 
+    *
+    FROM
+    movie
+    WHERE
+    movie_id = ${movieId};`;
+  const movie = await database.get(getMovieQuery);
+  response.send(convertMovieObjectToResponseObject(movie));
+});
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const { movieId } = request.params;
+  const updateMovieQuery = `
+    UPDATE
+    movie
+    SET
+    director_id = ${directorId},
+    movie_name = '${movieName}',
+    lead_actor = '${leadActor}'
+    WHERE
+    movie_id = ${movieId};`;
+
+  await database.run(updateMovieQuery);
+  response.send("Movie Details Updated");
+});
+
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const deleteMoviesQuery = `
+    DELETE from
+    movie
+    WHERE movie_id = ${movie_id};`;
+  await database.run(deleteMovieQuery);
+  response.send("Movie Removed");
+});
+
+app.get("/directors/", async (request, response) => {
+  const getDirectorsQuery = `
+    SELECT
+    *
+    FROM
+    director;`;
+  const directorsArray = await database.all(getDirectorsQuery);
+  response.send(
+    directorsArray.map((eachDirector) =>
+      convertDirectorObjectToResponseObject(eachDirector)
+    )
+  );
+});
+
+app.get("/directors/:directorId/movies/", async (request, response) => {
+  const { directorId } = request.params;
+  const getDirectorMoviesQuery = `
+    SELECT
+    movie_name
+    FROM
+    movie
+    WHERE
+    director_id = '${directorId}';`;
+  const moviesArray = await database.all(getDirectorMoviesQuery);
+  response.send(
+    moviesArray.map((eachMovie) => ({ movieName: eachMovie.movie_name }))
+  );
+});
+
 module.exports = app;
